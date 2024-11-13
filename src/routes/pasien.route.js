5532c9752801a3cfe05607bc898921fa4b4e462d
george <georgewk1809@gmail.com>
2024-11-13 20:35:21 +0000

    add pasien routes

---
+++ src/routes/pasien.route.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const PasienController = require('../controllers/pasien.controller')
+const router = express.Router()
+
+router.get('/', PasienController.getPasien)
+router.get('/:id', PasienController.getPasienById)
+router.post('/', PasienController.createPasien)
+router.patch('/:id', PasienController.updatePasien)
+router.delete('/:id', PasienController.deletePasien)
+
+module.exports = router
