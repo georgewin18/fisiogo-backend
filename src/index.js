0875103b228bb54c2518def1ab835315ea2ccbd1
george <georgewk1809@gmail.com>
2024-11-13 20:35:06 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const PasienRoutes = require('./routes/pasien.route')
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+  res.send('Welcome to FisioGo-API')
+})
+
+app.use('/api/pasien', PasienRoutes)
+
+mongoose.connect(process.env.CONN_STRING)
+.then(() => {
+  console.log('connected to database')
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+  })
+})
+.catch((error) => {
+  console.log(error)
+})
